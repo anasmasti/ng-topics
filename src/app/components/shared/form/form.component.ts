e74ff920f218e46dc753cee3aff6566ffc503e8a
anasmasti <anasmasti@hotmail.com>
2022-11-24 14:31:47 +0100

    Add doc

--- src/app/components/shared/form/form.component.ts
+++ src/app/components/shared/form/form.component.ts
@@ -20,11 +20,16 @@ export class FormComponent {
   }
 
   postData() {
+    // Check if Input has value
     if (this.inputData != '') {
+      // Update store
       this.store.dispatch(addMovie({ movie: this.inputData }));
+      // Pass data to the parent component
       this.formData.emit(this.inputData);
+      // Update the event to empty the input
       this.isSent = true;
 
+      // Reset values
       setTimeout(() => {
         this.isSent = false;
         this.inputData = '';
